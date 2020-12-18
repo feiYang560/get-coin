cf031774d823a9a9b79497fe04e8736a578adc06
feiYang560 <68410528+feiYang560@users.noreply.github.com>
2020-12-18 09:34:45 +0800

    Update get_coin.py

--- get_coin.py
+++ get_coin.py
@@ -6,6 +6,7 @@ from prettytable import PrettyTable
 
 setlocale(LC_NUMERIC, 'English_US')
 def getCoin():
+    '''获取币圈前100行情'''
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
     }
